9d2fba815ff42959607665f43d56cd3189d085c9
rere <creamynald@gmail.com>
2024-08-21 21:27:55 +0700

    fix: routing instansi

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\backend\dashboardController;
-use App\Http\Controllers\instansiController;
+use App\Http\Controllers\backend\settings\instansiController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::prefix('admin')->group(function () {
-    Route::get('dashboard', [dashboardController::class, 'index']);
+    Route::get('dashboard', [dashboardController::class, 'index'])->name('dashboard');
 
     // for super admin
     Route::prefix('settings')->group(function () {
