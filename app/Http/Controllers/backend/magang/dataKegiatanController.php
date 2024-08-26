70f3e8a3e82666c73905c5dce3728c05ca6a45a0
creamynald <creamynald@gmail.com>
2024-08-26 09:51:20 +0700

    add: data kegiatan

---
+++ app/Http/Controllers/backend/magang/dataKegiatanController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\backend\magang;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class dataKegiatanController extends Controller
+{
+    //
+}
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\backend\dashboardController;
+
+use App\Http\Controllers\backend\magang\rincianKegiatanController;
+use App\Http\Controllers\backend\magang\dataKegiatanController as dataKegiatanMagangController;
+
 use App\Http\Controllers\backend\settings\{instansiController, bidangInstansiController, kegiatanController, userController};
+use App\Http\Controllers\backend\settings\dataKegiatanController as dataKegiatanController;
+use App\Models\rincianKegiatan;
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,12 +17,17 @@ Route::get('/', function () {
 Route::prefix('admin')->group(function () {
     Route::get('dashboard', [dashboardController::class, 'index'])->name('dashboard');
 
+    Route::prefix('magang')->group(function () {
+        Route::resource('data-kegiatan', dataKegiatanMagangController::class);
+        Route::resource('rincian-kegiatan', rincianKegiatanController::class);
+    });
     // for super admin
     Route::prefix('settings')->group(function () {
         Route::resource('instansi', instansiController::class);
         Route::resource('bidang-instansi', bidangInstansiController::class);
         Route::resource('kegiatan', kegiatanController::class);
-
+        Route::resource('data-kegiatan', dataKegiatanController::class);
+        
         Route::resource('users', userController::class);
     });
 });
