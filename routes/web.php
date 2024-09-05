52bd4a82fdd7e906cb65329dfe551f55353e8d21
nia060803 <sityaminahh3@gmail.com>
2024-09-05 12:24:06 +0700

    add: module post

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\backend\dashboardController;
 
@@ -9,6 +10,10 @@ use App\Http\Controllers\backend\magang\dataKegiatanController as dataKegiatanMa
 use App\Http\Controllers\backend\settings\{instansiController, bidangInstansiController, kegiatanController, userController};
 use App\Http\Controllers\backend\settings\dataKegiatanController as dataKegiatanController;
 use App\Models\rincianKegiatan;
+=======
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\backend\settings\instansiController;
+>>>>>>> 9169589 (add: module post)
 
 Route::get('/', function () {
     return view('welcome');
@@ -17,17 +22,24 @@ Route::get('/', function () {
 Route::prefix('admin')->middleware('auth')->group(function () {
     Route::get('dashboard', [dashboardController::class, 'index'])->name('dashboard');
 
+<<<<<<< HEAD
     Route::prefix('magang')->group(function () {
         Route::resource('data-kegiatan', dataKegiatanMagangController::class);
         Route::resource('rincian-kegiatan', rincianKegiatanController::class);
     });
+=======
+//route resource
+Route::resource('/posts', PostController::class);
+
+
+>>>>>>> 9169589 (add: module post)
     // for super admin
     Route::prefix('settings')->group(function () {
         Route::resource('instansi', instansiController::class);
         Route::resource('bidang-instansi', bidangInstansiController::class);
         Route::resource('kegiatan', kegiatanController::class);
         Route::resource('data-kegiatan-magang', dataKegiatanController::class);
-        
+
         Route::resource('users', userController::class);
     });
 });
